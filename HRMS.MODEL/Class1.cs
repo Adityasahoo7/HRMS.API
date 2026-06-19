804b3cb1c02468625594bd7af14d0a092ff42e21
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-06-20 00:43:25 +0530

    Add Project structer

--- HRMS.MODEL/Class1.cs
+++ HRMS.MODEL/Class1.cs
@@ -1,4 +1,4 @@
-﻿namespace HRMS.MODELS
+﻿namespace HRMS.MODEL
 {
     public class Class1
     {
--- HRMS.MODELS/obj/Debug/net8.0/HRMS.MODELS.AssemblyInfo.cs
+++ HRMS.MODELS/obj/Debug/net8.0/HRMS.MODELS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMS.MODELS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e58432761ab9cf3e0f05fb7e2a98494f1ad0cafa")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMS.MODELS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMS.MODELS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
