470fb75398211eb2b2ff77370fa4b69fb4af5b27
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-06-30 12:21:20 +0530

    Add Auth DBSET

--- HRMS.DAL/HRMSDBcontext.cs
+++ HRMS.DAL/HRMSDBcontext.cs
@@ -1,4 +1,5 @@
 ﻿using HRMS.DAL.Models;
+using HRMS.DAL.Models.AUTH_MODULE;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,11 @@ namespace HRMS.DAL
 
         public DbSet<Employee> Employee { get; set; }
         public DbSet<BaseEntity> BaseEntity { get; set; }
+        //Auth Module
+        public DbSet<RefreshToken> RefreshToken { get; set; }
+        public DbSet<Role> Role { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<UserRole> UserRole { get; set; }
 
     }
 }
