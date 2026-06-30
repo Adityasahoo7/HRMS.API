42ba87a57b302229c4011e882ae837d182abc158
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-06-30 12:43:15 +0530

    Add Employee dbset

--- HRMS.DAL/HRMSDBcontext.cs
+++ HRMS.DAL/HRMSDBcontext.cs
@@ -16,13 +16,28 @@ namespace HRMS.DAL
                 
         }
 
-        public DbSet<Employee> Employee { get; set; }
+     
         public DbSet<BaseEntity> BaseEntity { get; set; }
         //Auth Module
-        public DbSet<RefreshToken> RefreshToken { get; set; }
-        public DbSet<Role> Role { get; set; }
-        public DbSet<User> User { get; set; }
-        public DbSet<UserRole> UserRole { get; set; }
+        public DbSet<RefreshToken> RefreshTokenDS { get; set; }
+        public DbSet<Role> RoleDS { get; set; }
+        public DbSet<User> UserDS { get; set; }
+        public DbSet<UserRole> UserRoleDS { get; set; }
+
+
+        //EMPLOYEE CORE
+
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<EmployeeBankDetails> EmployeeBankDetailsDS { get; set; }
+        public DbSet<EmployeeContactInfo> EmployeeContactInfoDS { get; set; }
+        public DbSet<EmployeeEducation> EmployeeEducationsDS { get; set; }
+
+        public DbSet<EmployeeEmployment> EmployeeEmploymentDS { get; set; }
+        public DbSet<EmployeePersonalInfo> EmployeePersonalInfoDS { get; set; }
+
+
+
+
 
     }
 }
